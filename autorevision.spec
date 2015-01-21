9a3ac4e0e28dfaa805c75415668870d58133f50b
Dennis Biringer <dbiringer@integrity-apps.com>
2015-01-21 03:05:42 +0000

    Added RPM spec file and script.
    Issue ID:

---
+++ autorevision.spec
@@ -0,0 +1,79 @@
+Name:           autorevision
+Version:        1.10a
+Release:        1%{?dist}
+Summary:        A shell script for extracting revision information.
+
+License:       dak180
+URL:           https://github.com/Autorevision/autorevision
+Source0:       %{name}-%{version}.tar.gz
+
+BuildRequires: asciidoc
+BuildRequires: coreutils
+BuildRequires: gnupg2
+
+
+%description
+A shell script for extracting revision information useful
+in release/build scripting from repositories.
+
+Supported repository types include `git`, `hg`, `bzr`, and `svn`. The
+record can be emitted in a ready-to-use form for `C`, `C++`, `Java`,
+`bash`, `Python`, `Perl`, `lua`, `php`, `ini` and others.
+
+Emitted information includes the ID of the most recent commit, its
+branch, its date, and several other useful pieces of meta-information.
+
+There is support for reading and writing a cache file so autorevision
+will remain useful during a build from an unpacked distribution
+tarball.
+
+See the [manual page](http://www.catb.org/esr/autorevision/autorevision.html),
+included in the distribution, for invocation details.
+
+You can check out examples of the different output that autorevision
+can produce in (https://github.com/Autorevision/autorevision/tree/master/examples).
+
+
+
+# These are the locations of the app-specific files.
+#
+%define app        autorevision
+%define _prefix    /usr
+%define bindir     %{_prefix}/bin
+%define sharedir   %{_prefix}/share
+%define docdir	   %{sharedir}/doc
+%define mandir	   %{sharedir}/man/man1
+
+
+%prep
+%setup -q
+
+%build
+
+%install
+mkdir -p $RPM_BUILD_ROOT%{bindir}
+mkdir -p $RPM_BUILD_ROOT%{docdir}/%{app}-%{version}
+mkdir -p $RPM_BUILD_ROOT%{mandir}
+
+cp %{app} $RPM_BUILD_ROOT%{bindir}/%{app}
+cp -r AUTHORS.txt %{app}.html contribs CONTRIBUTING.html \
+      COPYING.html examples NEWS README.html \
+      $RPM_BUILD_ROOT%{docdir}/%{app}-%{version}
+gzip --no-name < %{app}.1 > $RPM_BUILD_ROOT%{mandir}/%{app}.1.gz
+
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+
+%files
+%defattr(-,root,root,-)
+%{bindir}/%{app}
+%{docdir}/%{app}-%{version}/*
+%{mandir}/%{app}.1.gz
+
+
+%changelog
+* Wed Jan 21 2015 Dennis Biringer <dbiringer@integrity-apps.com> - 1.10a-1
+- Initial RPM for PDS extended autorevision.
+
---
+++ create_rpm.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+
+#
+# This script packages the files needed to build an RPM file
+# for autorevision. A standard RPM build environment
+# is required.
+#
+
+# RPMBUILD option
+STAGE=$1
+SHORT_CIRCUIT=$2
+
+APP=autorevision
+VERSION=1.10a
+PKG=${APP}-${VERSION}
+SPEC=${APP}.spec
+
+PKG_BASE=/tmp/rpm$$
+
+# Standard RPM build tree
+RPM_BUILD=$HOME/rpmbuild
+
+FILES="
+	AUTHORS.txt
+	autorevision
+	autorevision.1
+	autorevision.html
+	contribs
+	CONTRIBUTING.html
+	COPYING.html
+	examples
+	NEWS
+	README.html
+	"
+
+
+# Check for and make the standard setup
+if [ -x "/usr/bin/rpmdev-setuptree" ]
+then
+    [ -d $RPM_BUILD ] || /usr/bin/rpmdev-setuptree
+else
+    echo
+    echo "   You need to load the rpmdevtools package!"
+    echo
+    echo "   After that, rerun this script and the standard RPM build structure"
+    echo "   will be created for you in $RPM_BUILD."
+    echo
+    exit 1
+fi
+
+usage()
+{
+    echo
+    echo "   Usage: $(basename $0) [p | c | i | l | b | s | a] [short]"
+    echo
+    echo "          h : show help"
+    echo "          p : prep"
+    echo "          c : build"
+    echo "          i : install"
+    echo "          l : list check"
+    echo "          b : build binary package (default)"
+    echo "          s : build source package"
+    echo "          a : build binary and source package"
+    echo "      short : short-circuit step"
+    echo
+    exit 1
+}
+
+case "$STAGE" in
+
+    '') STAGE='b' ;;
+
+    h | -h | --help) usage ;;
+
+    p | c | i | l | b | s | a) break ;;
+
+    *) usage ;;
+
+esac
+
+[ "$SHORT_CIRCUIT" ] && SHORT_CIRCUIT='--short-circuit'
+
+trap "rm -rf $PKG_BASE; exit 0" EXIT ERR QUIT TERM
+
+
+# If short-circuiting, don't generate new build files
+if [ -z "$SHORT_CIRCUIT" ]
+then
+    # Cleanup source directory
+    make devclean
+    
+    # Pre-cleaning
+    mkdir -p $PKG_BASE/$PKG
+
+    # Needs a markdown converter installed (e.g. python-markdown)
+    make all docs
+    
+    # Copy the system files to the package
+    cp -r $FILES $PKG_BASE/$PKG
+    
+    # Make the source archive for rpmbuild
+    tar czf $RPM_BUILD/SOURCES/${PKG}.tar.gz -C $PKG_BASE $PKG
+
+    # Copy the spec file to the build location
+    cp autorevision.spec $RPM_BUILD/SPECS
+fi
+
+# Create package
+rpmbuild -b$STAGE $SHORt_CIRCUIT $RPM_BUILD/SPECS/$SPEC
+
